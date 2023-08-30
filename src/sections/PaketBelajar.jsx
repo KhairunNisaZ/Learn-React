175a3ae6de74bfad55c7733653e034f722ff935d
KhairunNisaZ <khairun.nisa.zuqri92@gmail.com>
2023-08-30 09:28:08 +0700

    feat: add container paket belajar

--- src/sections/PaketBelajar.jsx
+++ src/sections/PaketBelajar.jsx
@@ -1,7 +1,7 @@
 export default function PaketBelajar() {
   return (
     <section>
-      <div className="container mx-auto rounded bg-slate-200 p-16 mt-[-4rem]"></div>
+      <div className="max-w-[75rem] mx-auto rounded bg-slate-200 p-16 mt-[-4rem]"></div>
     </section>
   );
 }
